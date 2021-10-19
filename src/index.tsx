3a0cacc2f6c5cb1ea2a9c63cbb5b1dd64c3867c9
Fareman <misha.xarun2011@mail.ru>
2021-10-19 17:30:56 +0700

    feat: add git message check

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ыфвsad
+// нигас эфио лики блики стэфио
 ReactDOM.render(
   <React.StrictMode>
     <App />
